7211435455db3c09eb1561a097a915cf89e006c1
casasteguimedia-hub <casastegui.media@gmail.com>
2026-06-03 13:16:31 -0500

    Import mobile footer stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './mobile-fix.css'
+import './mobile-footer.css'
 import App from './App.jsx'
 import MobileHome from './MobileHome.jsx'
 
